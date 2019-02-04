126f467289ca493d48f8f00feab092891c84d664
Guilherme Ferreira <guilherme.ferreira29@hotmail.com>
2019-02-04 21:51:42 -0200

    detalhes dos produtos

--- vendor/tagui/php-classes/src/Model/Product.php
+++ vendor/tagui/php-classes/src/Model/Product.php
@@ -64,24 +64,21 @@ public function delete()
     ]);
 }
 public function checkPhoto()
-{
-    if (file_exists($_SERVER['DOCUMENT_ROOT']. DIRECTORY_SEPARATOR.
-     "res" . DIRECTORY_SEPARATOR.
-     "site" . DIRECTORY_SEPARATOR.
-     "img" . DIRECTORY_SEPARATOR.
-     "products" . DIRECTORY_SEPARATOR.
-     $this->getidproduct().".jpg"
-     )){
-
-         $url = "/res/site/img/products/" . $this->getidproduct().".jpg";
-
-     }else{
-
-        $url = "/res/site/img/products/sem-foto.jpg";
-     }
-
-     return $this->setdesphoto($url);
-}
+	{
+		if (file_exists(
+			$_SERVER['DOCUMENT_ROOT'] . DIRECTORY_SEPARATOR . 
+			"res" . DIRECTORY_SEPARATOR . 
+			"site" . DIRECTORY_SEPARATOR . 
+			"img" . DIRECTORY_SEPARATOR . 
+			"products" . DIRECTORY_SEPARATOR . 
+			$this->getidproduct() . ".jpg"
+			)) {
+			$url = "/res/site/img/products/" . $this->getidproduct() . ".jpg";
+		} else {
+			$url = "/res/site/img/product.jpg";
+		}
+		return $this->setdesphoto($url);
+	}
 
 public function getValues()
 {
@@ -127,6 +124,34 @@ public function setPhoto($file)
 
     $this->checkPhoto();
 }
+
+public function getFromURL($desurl)
+{
+    $sql = new Sql();
+    
+    $rows = $sql -> select("select * from tb_products where desurl = :desurl limit 1 ", [
+        ':desurl' => $desurl
+    ]);
+
+    $this->setData($rows[0]);
+
+
+}
+
+public function getCategories()
+{
+    $sql = new Sql();
+
+    return $sql->select("
+        select * from tb_categories a inner join tb_productscategories  b on a.idcategory = b.idcategory where b.idproduct = :idproduct
+    ",[
+        ':idproduct'=> $this->getidproduct()
+    ]);
+}
+
+
+
+
 
 }//fim
 ?>
