23a2fe0532e77c60b500114c7e78bba2a23cef13
Guilherme Ferreira <guilherme.ferreira29@hotmail.com>
2019-03-30 19:54:13 -0300

    Finalizar pedido - Boleto

--- site.php
+++ site.php
@@ -6,6 +6,8 @@ use \Tagui\Model\Category;
 use \Tagui\Model\Cart;
 use \Tagui\Model\User;
 use \Tagui\Model\Address;
+use \Tagui\Model\Order;
+use \Tagui\Model\OrderStatus;
 
 $app->get('/', function() {
     
@@ -187,9 +189,20 @@ $app->post('/checkout', function() {
 	$address->setData($_POST);
 
 	$address->save();
+	$cart = Cart :: getFromSession();
+	$totals = $cart->getCalculateTotal();
 
-header("Location: /order");
-exit;
+	$order = new Order();
+	$order->setData([
+		'idcart'=>$cart->getidcart(),
+		'idaddress'=>$address->getidaddress(),
+		'iduser'=>$user->getiduser(),
+		'idstatus'=>OrderStatus::EM_ABERTO,
+		'vltotal'=>$cart->getvltotal()
+	]);
+	$order->save();
+	header("Location: /order/".$order->getidorder());
+	exit;
 });
 
 $app->get('/checkout', function() {
@@ -434,4 +447,82 @@ $app->post("/profile", function(){
 });
 
 
+$app->get("/order/:idorder", function($idorder){
+	User::verifyLogin(false);
+	$order = new Order();
+	$order->get((int)$idorder);
+	$page = new Page();
+	$page->setTpl("payment", [
+		'order'=>$order->getValues()
+	]);
+});
+
+$app->get("/boleto/:idorder", function($idorder){
+
+	User::verifyLogin(false);
+	$order = new Order();
+	$order->get((int)$idorder);
+	
+// DADOS DO BOLETO PARA O SEU CLIENTE
+$dias_de_prazo_para_pagamento = 10;
+$taxa_boleto = 5.00;
+$data_venc = date("d/m/Y", time() + ($dias_de_prazo_para_pagamento * 86400));  // Prazo de X dias OU informe data: "13/04/2006"; 
+$valor_cobrado = formatPrice($order->getvltotal()); // Valor - REGRA: Sem pontos na milhar e tanto faz com "." ou "," ou com 1 ou 2 ou sem casa decimal
+$valor_cobrado = str_replace(",", ".",$valor_cobrado);
+$valor_boleto=number_format($valor_cobrado+$taxa_boleto, 2, ',', '');
+
+$dadosboleto["nosso_numero"] = $order->getidorder();  // Nosso numero - REGRA: Máximo de 8 caracteres!
+$dadosboleto["numero_documento"] = $order->getidorder();	// Num do pedido ou nosso numero
+$dadosboleto["data_vencimento"] = $data_venc; // Data de Vencimento do Boleto - REGRA: Formato DD/MM/AAAA
+$dadosboleto["data_documento"] = date("d/m/Y"); // Data de emissão do Boleto
+$dadosboleto["data_processamento"] = date("d/m/Y"); // Data de processamento do boleto (opcional)
+$dadosboleto["valor_boleto"] = $valor_boleto; 	// Valor do Boleto - REGRA: Com vírgula e sempre com duas casas depois da virgula
+
+/// DADOS DO SEU CLIENTE
+$dadosboleto["sacado"] = $order->getidsperson();
+$dadosboleto["endereco1"] = $order->getdesaddress() . " " . $order->getdestrict();
+$dadosboleto["endereco2"] =  $order->getdescity() . " - " . $order->getdesstate() . $order->getdescountry() . " - CEP: " . $order->getdeszipcode();
+
+
+// INFORMACOES PARA O CLIENTE
+$dadosboleto["demonstrativo1"] = "Pagamento de Compra na Loja tagui store E-commerce";
+$dadosboleto["demonstrativo2"] = "Taxa bancária - R$ 0,00";
+$dadosboleto["demonstrativo3"] = "";
+$dadosboleto["instrucoes1"] = "- Sr. Caixa, cobrar multa de 2% após o vencimento";
+$dadosboleto["instrucoes2"] = "- Receber até 10 dias após o vencimento";
+$dadosboleto["instrucoes3"] = "- Em caso de dúvidas entre em contato conosco: suporte@taguistore.com.br";
+$dadosboleto["instrucoes4"] = "&nbsp; Emitido pelo sistema taguistore E-commerce - www.taguistore.com.br";
+
+// DADOS OPCIONAIS DE ACORDO COM O BANCO OU CLIENTE
+$dadosboleto["quantidade"] = "";
+$dadosboleto["valor_unitario"] = "";
+$dadosboleto["aceite"] = "";		
+$dadosboleto["especie"] = "R$";
+$dadosboleto["especie_doc"] = "";
+
+
+// ---------------------- DADOS FIXOS DE CONFIGURAÇÃO DO SEU BOLETO --------------- //
+
+
+// DADOS DA SUA CONTA - ITAÚ
+$dadosboleto["agencia"] = "1690"; // Num da agencia, sem digito
+$dadosboleto["conta"] = "48781";	// Num da conta, sem digito
+$dadosboleto["conta_dv"] = "2"; 	// Digito do Num da conta
+
+// DADOS PERSONALIZADOS - ITAÚ
+$dadosboleto["carteira"] = "175";  // Código da Carteira: pode ser 175, 174, 104, 109, 178, ou 157
+
+// SEUS DADOS
+$dadosboleto["identificacao"] = "Tagui Store";
+$dadosboleto["cpf_cnpj"] = "0001";
+$dadosboleto["endereco"] = "Rua teste";
+$dadosboleto["cidade_uf"] = "Passos MG";
+$dadosboleto["cedente"] = "Tagui Store";
+
+// NÃO ALTERAR!
+$path = $_SERVER['DOCUMENT_ROOT'] . DIRECTORY_SEPARATOR . "res" . DIRECTORY_SEPARATOR . "boletophp" . DIRECTORY_SEPARATOR . "include" . DIRECTORY_SEPARATOR; 
+require_once($path . "funcoes_itau.php"); 
+require_once($path . "layout_itau.php");
+
+});
 ?>
