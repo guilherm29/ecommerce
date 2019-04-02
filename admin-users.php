fcca1ac939f3c19e100ebf839e0878e54481e460
Guilherme Ferreira <guilherme.ferreira29@hotmail.com>
2019-04-01 21:10:13 -0300

    admin-user paginaçõ

--- admin-users.php
+++ admin-users.php
@@ -4,26 +4,34 @@ use Tagui\PageAdmin;
 use Tagui\Model\User;
 
 $app->get("/admin/users", function() {
-
-User::verifyLogin();
-
-$users = User::listAll();
-
-
-$page = new PageAdmin(); 
-
-$page->setTpl("users", array(
-    "users"=>$users
-     
-));
-
-});
-
-$app->get("/admin/users/create", function() {
-
     User::verifyLogin();
-    $page = new PageAdmin();
-    $page-> setTpl("users-create");
+
+    $search = (isset($_GET['search'])) ? $_GET['search'] : "";
+    
+    $page = (isset($_GET['page'])) ? (int)$_GET['page'] : 1;
+    
+	if ($search != '') {
+		$pagination = User::getPageSearch($search, $page);
+	} else {
+		$pagination = User::getPage($page);
+	}
+	$pages = [];
+	for ($x = 0; $x < $pagination['pages']; $x++)
+	{
+		array_push($pages, [
+			'href'=>'/admin/users?'.http_build_query([
+				'page'=>$x+1,
+				'search'=>$search
+			]),
+			'text'=>$x+1
+		]);
+	}
+	$page = new PageAdmin();
+	$page->setTpl("users", array(
+		"users"=>$pagination['data'],
+		"search"=>$search,
+		"pages"=>$pages
+	));
 });
 
 
